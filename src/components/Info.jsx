be71b79284dab6366795b24205c9141080b7371b
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-27 13:27:58 +0100

    moved images to public folder

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,7 +2,7 @@ export default function Info() {
     return (
         <>
             <div className="info">
-                <img className="profile-pic" src="src/assets/profile-pic.jpeg"/>
+                <img className="profile-pic" src="/profile-pic.jpeg"/>
                 <div className="info--details">
                     <h1>Dimitri Chupryna</h1>
                     <p>Entrepreneur</p>
@@ -12,19 +12,19 @@ export default function Info() {
                 <div className="info--social">
                     <a href="https://t.me/dimitrichupryna" target="_blank">
                         <div className="button">
-                            <img className="social-icon" src="src/assets/telegram-icon.png" />
+                            <img className="social-icon" src="/telegram-icon.png" />
                             <p>Telegram</p>
                         </div>
                     </a>
                     <a href="https://www.linkedin.com/in/dchupryna/" target="_blank">
                         <div className="button">
-                            <img className="social-icon" src="src/assets/linkedin-icon.png" />
+                            <img className="social-icon" src="/linkedin-icon.png" />
                             <p>LinkedIn</p>
                         </div>
                     </a>
                     <a href="https://cal.com/dimitri-c/30min" target="_blank">
                         <div className="button">
-                            <img className="social-icon" src="src/assets/call-icon.png" />
+                            <img className="social-icon" src="call-icon.png" />
                             <p>Call</p>
                         </div>
                     </a>
